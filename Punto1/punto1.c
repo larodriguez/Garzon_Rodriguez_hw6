0ed96acc20c39e13f576d7862260012d5868fea3
larodriguez <la.rodriguez1876@uniandes.edu.co>
2014-10-24 17:20:27 -0500

    adelanto4orden

--- Punto1/punto1.c
+++ Punto1/punto1.c
@@ -4,38 +4,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define A 20.0
+#define B 1.0
+#define C 30.0
+#define D 1.0
 
 /*LLAMA LAS FUNCIONES DEFINIDAS**/
-float derivada_x (float x, float y, float t);
-float derivada_y (float x, float y, float t);
+float derivada_x (float t, float x, float y);
+float derivada_y (float t, float x, float y);
 
 /*MAIN*/
 int main (int argc, char **argv){ 
 
   /*DEFINICION DE LAS VARIABLES*/
-  float h = 0.01;
+  float h = 1.0E-3;
   float t_min = 0.0;
   float t_max = 1.0;
-  float A = 20.0;
-  float B = 1.0;
-  float C = 30.0;
-  float D = 1.0;
 
-  float n_puntos = ((t_max - t_min)/h);
+  int n_puntos = (int)((t_max - t_min)/h);
 
-  int y0;
-  int x0;
+  float y0;
+  float x0;
   int i;
 
-  float x_prime;
-  float y_prime;
-
-  float t_middle; 
-  float x_middle;
-  float y_middle;
-
-  float x_middle_prime;
-  float y_middle_prime;
+  float t1;
+  float t2;
+  float t3;
+  float t4;
+  float x1;
+  float x2;
+  float x3;
+  float x4;
+  float y1;
+  float y2;
+  float y3;
+  float y4;
+  float k1;
+  float k2;
+  float k3;
+  float k4;
+  float u1;
+  float u2;
+  float u3;
+  float u4;
+  float average_k;
+  float average_u;
 
   FILE *data;
     
@@ -48,7 +61,7 @@ int main (int argc, char **argv){
   y = malloc(n_puntos*sizeof(float));
   t = malloc(n_puntos*sizeof(float));
 
-  /*LLENAMOS DE O.0 TODOS 3 PUNTEROS*/
+  /*LLENAMOS DE 0.0 TODOS 3 PUNTEROS*/
   for(i=0;i<n_puntos;i++){
     x[i] = 0.0;
     y[i] = 0.0;
@@ -56,29 +69,46 @@ int main (int argc, char **argv){
   }
   
   /*RECIBIMOS POR PARAMETRO LA CONDICION INICIAL*/
-  x0 = atoi(argv[1]);
-  y0 = atoi(argv[2]);
+  x0 = atof(argv[1]);
+  y0 = atof(argv[2]);
 
   /*ASIGNAMOS LAS CONDICIONES INICIALES*/
   t[0] = t_min;
-  x[0] = y0;
-  y[0] = x0;
+  x[0] = x0;
+  y[0] = y0;
 
-  /*RESOLVEMOS LAS ECUACIONES ACOPLADAS POR EL METODO RUNGE-KUTTA DE 2DO ORDEN*/
+  /*RESOLVEMOS LAS ECUACIONES ACOPLADAS POR EL METODO RUNGE-KUTTA DE 4TO ORDEN*/
   for(i=1;i<n_puntos;i++){
-    x_prime = derivada_x(t[i-1],x[i-1],y[i-1]);
-    y_prime = derivada_y(t[i-1],x[i-1],y[i-1]);
-
-    t_middle = t[i-1] + (h/2.0);
-    x_middle = ((x[i-1] + (h/2.0))*x_prime);
-    y_middle = ((y[i-1] + (h/2.0))*y_prime);
-
-    x_middle_prime = derivada_x(t_middle,x_middle,y_middle);
-    y_middle_prime = derivada_y(t_middle,x_middle,y_middle);
-
+    
+    k1 = derivada_x(t[i-1],x[i-1],y[i-1]);
+    u1 = derivada_y(t[i-1],x[i-1],y[i-1]);
+    /* Primer paso*/
+    t1 = t[i-1] + (h/2.0);
+    x1 = x[i-1] + ((h/2.0)*k1);
+    y1 = y[i-1] + ((h/2.0)*u1);
+    k2 = derivada_x(t1, x1, y1);
+    u2 = derivada_y(t1, x1, y1);
+    /*Segundo paso*/
+    t2 = t[i-1] + (h/2.0);
+    x2 = x[i-1] + ((h/2.0)*k2);
+    y2 = y[i-1] + ((h/2.0)*u2);
+    k3 = derivada_x(t2, x2, y2);
+    u3 = derivada_y(t2, x2, y2);
+    /*Tercer paso*/    
+    t3 = t[i-1] + h;
+    x3 = x[i-1] + (h*k3);
+    y3 = y[i-1] + (h*u3);
+    k4 = derivada_x(t3, x3, y3);
+    u4 = derivada_y(t3, x3, y3);
+    /*Cuarto paso*/
+   
+    average_k = (1.0/6.0)*(k1 + (2.0*k2) + (2.0*k3) + k4);
+    average_u = (1.0/6.0)*(u1 + (2.0*u2) + (2.0*u3) + u4);
+    
     t[i] = t[i-1] + h;
-    x[i] = (x[i-1] + h)*x_middle_prime;
-    y[i] = (y[i-1] + h)*y_middle_prime;
+    x[i] = x[i-1] + (h*average_k);
+    y[i] = y[i-1] + (h*average_u);
+    
   }
 
   /*GENERAMOS EL ARCHIVO .DAT QUE ALMACENA LOS DATOS*/
@@ -89,31 +119,21 @@ int main (int argc, char **argv){
     fprintf(data,"%f %f %f \n", t[i], x[i], y[i]);
   }
   fclose(data);
- 
+  return 0;
 }
 
 /*DEFINIMOS LA FUNCION derivada_x COMO LA PRIMERA ECUACION DIFERENCIAL DEL PROBLEMA*/
 float derivada_x (float t, float x, float y){
 
-  float A = 20.0;
-  float B = 1.0;
-  float C = 30.0;
-  float D = 1.0;
-  float deri;
-
-  deri = (A*x) - (B*x*y);
-  return deri;
+  float derix;
+  derix = (A*x) - ((B*x)*y);
+  return derix;
 }
 
 /*DEFINIMOS LA FUNCION derivada_y COMO LA SEGUNDA ECUACION DIFERENCIAL DEL PROBLEMA*/
 float derivada_y (float t, float x, float y){
 
-  float A = 20.0;
-  float B = 1.0;
-  float C = 30.0;
-  float D = 1.0;
-  float deri;
-
-  deri = -(C*x) + (D*x*y);
-  return deri;
+  float deriy;
+  deriy = -(C*x) + ((D*x)*y);
+  return deriy;
 }
