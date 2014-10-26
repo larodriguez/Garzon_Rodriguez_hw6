169861427dd2ca4b437e6d9c927ebf28f5e54e31
Sebastian Garzon <js.garzon398@uniandes.edu.co>
2014-10-25 20:45:14 -0500

    arreglando titulos

--- Punto1/plot_poblaciones.py
+++ Punto1/plot_poblaciones.py
@@ -18,16 +18,19 @@ t = data[:,0]
 x = data[:,1]
 y = data[:,2]
 
+#TOMA LOS VALORES DE Xo y Yo
+
+x0 = data[0,1]
+y0 = data[0,2]
+
 #GENERA LA GRAFICA DE (X,Y)
 plt.figure(figsize=(15, 10))
 plt.plot (x,y,c=colors)
-plt.title(r'$Grafica\ de\ Presas\ vs\ Cazadores$', fontsize=20)
+plt.title(r'$Grafica\ de\ Presas\ vs\ Cazadores.\ Xo=\ '+str(x0)+'\ Yo=\ '+str(y0)+'$', fontsize=20)
 plt.xlabel(r'$Presas$', fontsize=20)
 plt.ylabel(r'$Cazadores$', fontsize=20)
 plt.grid()
 
-x0 = data[0,1]
-y0 = data[0,2]
 
 #GUARDA LA IMAGEN COMO .PDF
 plt.savefig("Poblaciones_"+str(int(x0))+"_"+str(int(y0))+".pdf", format='pdf', bbox_inches='tight', transparent=True)
