32f4b63fc7946a0de4c135833511be7d81a00c00
Sebastian Garzon <js.garzon398@uniandes.edu.co>
2014-10-27 12:20:56 -0500

    Empezando

--- Punto2/Punto2.c
+++ Punto2/Punto2.c
@@ -1,1 +1,15 @@
 /*SEBASTIAN GARZON - ALEJANDRO RODRIGUEZ*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define pi 3.14159265
+
+int main (int argc, char **argv){
+
+    k = atof(argv[1]);  /*Energia cinetica en megaelectronvoltios*/
+    alpha = atof(argv[2]);  /*alpha en grados*/
+    alpha_rad= (alpha*pi)/180 /*alpha en radianes*/
+    k_jul=(1.60217733*(10E-13)*k)/1 /*Energia cinetica en julios*/
+    
+}
+
