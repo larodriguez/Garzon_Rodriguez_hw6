6a6f52ce6103be25967c5b7e9d3ee095eff00eb7
larodriguez <la.rodriguez1876@uniandes.edu.co>
2014-10-30 13:26:17 -0500

    casi terminado

--- Punto2/Punto2.c
+++ Punto2/Punto2.c
@@ -15,7 +15,7 @@
 
 /*LLAMA LAS FUNCIONES DEFINIDAS*/
 
-void get_a(float rx, float ry, float rz, float vx, float vy, float vz,float* a);
+void get_a(float rx, float ry, float rz, float vx, float vy, float vz,float* a, float V);
 
 /*MAIN*/
 int main (int argc, char **argv){
@@ -100,7 +100,7 @@ int main (int argc, char **argv){
     
 
     /*Primer paso*/
-    get_a(r[0],r[1],r[2],v[0],v[1],v[2],a1);
+    get_a(r[0],r[1],r[2],v[0],v[1],v[2],a1,V);
 
 
     v1[0] = v[0] + ((h/2)*a1[0]);
@@ -111,7 +111,7 @@ int main (int argc, char **argv){
     r1[2] = r[2] + ((h/2)*v1[2]);
 
      /*Segundo paso*/
-    get_a(r1[0],r1[1],r1[2],v1[0],v1[1],v1[2],a2);
+    get_a(r1[0],r1[1],r1[2],v1[0],v1[1],v1[2],a2,V);
     
     v2[0] = v[0] + ((h/2)*a2[0]);
     v2[1] = v[1] + ((h/2)*a2[1]);
@@ -122,7 +122,7 @@ int main (int argc, char **argv){
 
     /*Tercer paso*/
     
-    get_a(r2[0],r2[1],r2[2],v2[0],v2[1],v2[2],a3);
+    get_a(r2[0],r2[1],r2[2],v2[0],v2[1],v2[2],a3,V);
     
     v3[0] = v[0] + (h*a3[0]);
     v3[1] = v[1] + (h*a3[1]);
@@ -132,7 +132,7 @@ int main (int argc, char **argv){
     r3[2] = r[2] + (h*v3[2]);
 
     /*Cuarto paso*/
-    get_a(r3[0],r3[1],r3[2],v3[0],v3[1],v3[2],a4);
+    get_a(r3[0],r3[1],r3[2],v3[0],v3[1],v3[2],a4,V);
     
     a[0] = (1.0/6.0)*(a1[0] + (2.0*a2[0]) + (2.0*a3[0]) + a4[0]);
     a[1] = (1.0/6.0)*(a1[1] + (2.0*a2[1]) + (2.0*a3[1]) + a4[1]);
@@ -156,18 +156,21 @@ int main (int argc, char **argv){
 }
 
 
-void  get_a(float rx, float ry, float rz, float vx, float vy, float vz,float* a){
+void  get_a(float rx, float ry, float rz, float vx, float vy, float vz,float* a, float V){
 
   float rr = sqrt((pow(rx,2))+(pow(ry,2))+(pow(rz,2)));
   
   float L = (B0*(pow(Rt,3)))/(pow(rr,5));
   
+  float gamma;
 
-  a[0] = q*((vy*2*L*(pow(rz,2)))-(vy*L*(pow(rx,2)))-(vy*L*(pow(ry,2)))-(vz*L*3*ry*rz));
+  gamma = 1/(sqrt(1-(pow((V/c),2))));
+
+  a[0] = (q/(gamma*m))*((vy*2*L*(pow(rz,2)))-(vy*L*(pow(rx,2)))-(vy*L*(pow(ry,2)))-(vz*L*3*ry*rz));
   
-  a[1] = q*(-(vx*L*2*(pow(rz,2)))+(vx*L*(pow(rx,2)))+(vx*L*(pow(ry,2)))+(vz*L*3*rx*rz));
+  a[1] = (q/(gamma*m))*(-(vx*L*2*(pow(rz,2)))+(vx*L*(pow(rx,2)))+(vx*L*(pow(ry,2)))+(vz*L*3*rx*rz));
 
-  a[2] = q*((vx*L*3*ry*rz)-(vy*L*3*rx*rz));
+  a[2] = (q/(gamma*m))*((vx*L*3*ry*rz)-(vy*L*3*rx*rz));
  
 }
 
